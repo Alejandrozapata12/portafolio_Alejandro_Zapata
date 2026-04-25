a189704bdcba20e59d01ebdcc9b871a3fd9ed8cb
Alejandrozapata12 <alejandrozapt152@gmail.com>
2026-04-24 20:44:47 -0500

    24/04/2026

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -12,36 +7,36 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import AIChat from "./components/AIChat";
-import { motion, useScroll, useSpring } from "motion/react";
+
+import { motion, useScroll, useSpring } from "framer-motion";
 import { LanguageProvider } from "./constants/LanguageContext";
 
 export default function App() {
   const { scrollYProgress } = useScroll();
+
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
-    restDelta: 0.001
+    restDelta: 0.001,
   });
 
   return (
     <LanguageProvider>
-      <div className="relative selection:bg-primary/30 grid-pattern min-h-screen overflow-x-hidden">
-        {/* Scroll Progress Bar */}
+      <div className="relative min-h-screen overflow-x-hidden">
         <motion.div
           className="fixed top-0 left-0 right-0 h-1 bg-primary z-[100] origin-left"
           style={{ scaleX }}
         />
 
         <Header />
-        
-        <main className="relative z-10 w-full overflow-hidden">
+
+        <main className="relative z-10 w-full">
           <Hero />
-          
-          {/* Section Divider */}
-          <div className="relative h-24 flex items-center justify-center">
-            <div className="w-[1px] h-full bg-gradient-to-b from-primary/20 to-transparent" />
+
+          <div className="h-24 flex items-center justify-center">
+            <div className="w-px h-full bg-gradient-to-b from-primary/20 to-transparent" />
           </div>
-          
+
           <About />
           <Experience />
           <Expertise />
@@ -55,5 +50,3 @@ export default function App() {
     </LanguageProvider>
   );
 }
-
-
